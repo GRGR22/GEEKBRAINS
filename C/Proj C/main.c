63a3a2b738b8d55047bed0bb3a03a1bfdc7508bd
GRGR22 <72486463+GRGR22@users.noreply.github.com>
2021-01-12 14:50:28 +0400

    Uploading manualy

---
+++ C/Proj C/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+int input;
+printf("Input your penis inlargement");
+scanf ("%d", &input);
+if (input >= 18)
+    printf("Your penis is very goooood");
+
+else
+    printf("Your penis is too small");
+
+return 0;
+}
+
---
+++ C/vim.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+int input;
+printf("Enter number");
+scanf("%d, &input");
+printf("Ypu entered number %d, we are doubled them for you%d", input, input*2)
+
+return 0;
+}
