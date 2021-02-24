5c941dd123733027197d4f84719c0e934d8ddc65
GR22 <bagbox@mail.ru>
2021-02-24 16:07:06 +0400

    super multu arr summ task

--- JS_lessons/task.js
+++ JS_lessons/task.js
@@ -1,9 +1,10 @@
-let arr = [[1, 2, 3], [4, 5], [6]];
+let arr = [[[1, 2], [3, 4]], [[5, 6], [7, 8]]];
 let summ = 0;
     
     for (i = 0; i < arr.length; i++) {  
         for (j = 0; j < arr[i].length; j++)  {
-        summ+=arr[i] [j]
+            for (k = 0; k < arr[i] [j].length; k++)
+            summ+=arr[i] [j] [k];
         }     
     }
 console.log(summ);
