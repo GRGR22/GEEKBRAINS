0138b5396c79d5070a3db22679b588d8fe3dacfd
GR22 <bagbox@mail.ru>
2021-02-24 15:37:22 +0400

    revers arr

--- JS_lessons/task.js
+++ JS_lessons/task.js
@@ -1,10 +1,7 @@
-let arr = [1, 2, 3, 1, 1, 11, 2, 1];
-let count = 0;
-let summ = 0;
+let arr = [1, 2, 3, 4, 5, 11, 7, 8];
+let result = [];
     
-    for (i = 0; i < arr.length; i++) {    
-        summ+=arr[i];
-        count++
-        if (summ >= 10) break;        
+    for (i = arr.length-1; i >= 0; i--) {    
+        result.push(arr[i])        
     }
-console.log(count);
+console.log(result);
