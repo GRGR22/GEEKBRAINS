a9b052585b375481da6e408822c3dbba154eeb74
GR22 <bagbox@mail.ru>
2021-02-24 14:53:56 +0400

    arr work

--- JS_lessons/task.js
+++ JS_lessons/task.js
@@ -1,10 +1,8 @@
-let arr=['a', 'e', 'b', 'c', 'a', 'a', 'b', 'e', 'e', 'b'];
-        let count = {};
-        for (i=0; i<arr.length; i++) {
-            if (count[arr[i]] === undefined) {
-                count[arr[i]] = 1
-            } else {
-                count[arr[i]]++
-            }            
-        }
-        console.log(count);
+let arr = [];
+let elem = '';
+
+for (i = 0; i < 3; i++) {
+	elem+='X'
+    arr.push(elem)
+}
+console.log(arr)
