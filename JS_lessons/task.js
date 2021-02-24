0a31bb8064efaab256b2af4539111ae18e51929c
GR22 <bagbox@mail.ru>
2021-02-24 15:33:07 +0400

    arr task

--- JS_lessons/task.js
+++ JS_lessons/task.js
@@ -1,8 +1,10 @@
-let arr = [];
-function arrayFill (elem, elemNumber) {
-    for (i = 0; i < elemNumber; i++) {    
-        arr.push(elem);
+let arr = [1, 2, 3, 1, 1, 11, 2, 1];
+let count = 0;
+let summ = 0;
+    
+    for (i = 0; i < arr.length; i++) {    
+        summ+=arr[i];
+        count++
+        if (summ >= 10) break;        
     }
-    return arr;
-}
-console.log(arrayFill('T', 52));
+console.log(count);
