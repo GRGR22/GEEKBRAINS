f52a8a30cd59939e18fa916a7075074e29fbb5be
GR22 <bagbox@mail.ru>
2021-02-25 13:35:24 +0400

    funk isNumberInRange

--- JS_lessons/task.js
+++ JS_lessons/task.js
@@ -1,10 +1,10 @@
-let arr = [[[1, 2], [3, 4]], [[5, 6], [7, 8]]];
-let summ = 0;
-    
-    for (i = 0; i < arr.length; i++) {  
-        for (j = 0; j < arr[i].length; j++)  {
-            for (k = 0; k < arr[i] [j].length; k++)
-            summ+=arr[i] [j] [k];
-        }     
-    }
-console.log(summ);
+var num = parseInt(prompt('Inter the number'));
+
+function isNumberInRange(num) {
+	if (num > 0 && num < 10) {
+		return true;
+	} else {
+		return false;
+	}
+}
+console.log(isNumberInRange(num));
