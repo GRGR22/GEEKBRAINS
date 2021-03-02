024d4e917b9b7df1da212b6d3681a2b57bca2d68
GR22 <bagbox@mail.ru>
2021-03-02 15:49:07 +0400

    duplicates of arr

--- JS_lessons/task.js
+++ JS_lessons/task.js
@@ -1,9 +1,8 @@
-let arr = [];
-let num = getDivisors(Number(prompt('Enter number to detect Divisors')));
-
-function getDivisors (toDivise) {
-    for (i=1; i <=toDivise; i++) {
-        if (toDivise % i === 0) arr.push(i)
+// let a = [['Goodbye'], {'Great': 'Job'}];
+let b = [1234, 1, 2, 3, 1234, 1234, 4, 5, 5, 5, 2, 1, 3, 66, 6, 66, 66, 7];
+let newArr = [];
+distinct(b);
+function distinct(a){
+    let z = [...new Set(a)]
+    console.log(z);
     }
-};
-console.log(arr)
